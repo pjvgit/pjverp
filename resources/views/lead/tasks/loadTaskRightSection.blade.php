b8ee2c80abe39af628c69c8685817dd9cf0f440a
Jignesh Prajapati <jignesh.prajapati@plutustec.com>
2021-07-26 14:08:10 +0530

    Fix:checkbox-error

--- resources/views/lead/tasks/loadTaskRightSection.blade.php
+++ resources/views/lead/tasks/loadTaskRightSection.blade.php
@@ -228,6 +228,11 @@
             if ($(this).prop('checked') == false) {
                 $("#linked_staff_checked_attend_" + id).prop('checked', $(this).prop('checked'));
             }
+            if ($('.client_attend_all_users:checked').length == $('.client_attend_all_users').length) {
+                $('.client_attend_all').prop('checked', true);
+            } else {
+                $('.client_attend_all').prop('checked', false);
+            }
             if ($("input:checkbox#time_tracking_enabled").is(":checked")) {
                 var SU = getCheckedUser();
                 loadTimeEstimationUsersList(SU);
