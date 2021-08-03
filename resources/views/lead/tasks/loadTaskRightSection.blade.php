828574103829ee33a175e883d03454cdf4face03
Jignesh Prajapati <jignesh.prajapati@plutustec.com>
2021-08-03 17:43:37 +0530

    Fix:lead-add-task-desing-error

--- resources/views/lead/tasks/loadTaskRightSection.blade.php
+++ resources/views/lead/tasks/loadTaskRightSection.blade.php
@@ -57,7 +57,7 @@
     <?php } ?>
     </div>
 </div>
-59
+
 <button type="button" class="btn btn-link" bladeName="resources/views/lead/tasks/loadTaskRightSection.blade.php" id="HideShowNonlink">Include staff member not linked to this case</button>
 <div class="sharing-table staff-table-nonlinked" @if(count($caseNonLinkedAssigned) == 0) style="display:none;" @endif>
     <div class="table-responsive">
