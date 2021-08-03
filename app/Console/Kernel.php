f3878707725f32ec3d19eeb020e0059edc63cf14
Trupti Bhuva <trupti.bhuva@plutustec.com>
2021-08-03 11:29:20 +0530

    event/task reminder email

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,10 +25,10 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('eventhour:reminderemail')->everyMinute();
-        $schedule->command('eventminute:reminderemail')->everyMinute();
-        $schedule->command('eventday:reminderemail')->everyThirtyMinutes();
-        $schedule->command('taskday:reminderemail')->everyThirtyMinutes();
+        // $schedule->command('eventhour:reminderemail')->everyMinute();
+        // $schedule->command('eventminute:reminderemail')->everyMinute();
+        // $schedule->command('eventday:reminderemail')->everyThirtyMinutes();
+        // $schedule->command('taskday:reminderemail')->everyThirtyMinutes();
         // $schedule->command('invoice:reminderemail')->everyThirtyMinutes();
     }
 
