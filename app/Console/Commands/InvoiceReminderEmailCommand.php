7483301a7a7d7ee8704b003f7823686ab9139938
Trupti Bhuva <trupti.bhuva@plutustec.com>
2021-10-28 20:06:02 +0530

    invoice email reminder, client portal and bug fixing

--- app/Console/Commands/InvoiceReminderEmailCommand.php
+++ app/Console/Commands/InvoiceReminderEmailCommand.php
@@ -114,7 +114,7 @@ class InvoiceReminderEmailCommand extends Command
                             foreach($item->invoiceSharedUser as $userkey => $useritem) {
                                 $date = Carbon::now($useritem->user_timezone ?? 'UTC'); // Carbon::now('Europe/Moscow'), Carbon::now('Europe/Amsterdam') etc..
                                 Log::info($useritem->user_timezone."=".$date);
-                                if ($date->hour == 10 || $date->hour == 15) { 
+                                if ($date->hour === 05) { 
                                     if($emailTemplate) {
                                         Log::info("invoice day time true");
                                         dispatch(new InvoiceReminderEmailJob($item, $useritem, $emailTemplate, $remindType, $days));
