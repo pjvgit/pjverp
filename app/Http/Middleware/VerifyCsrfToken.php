f27a4a580252e4fa43cb255ff8cf5f1f560948db
Trupti Bhuva <trupti.bhuva@plutustec.com>
2021-12-23 17:39:59 +0530

    invoice online payment webhook

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        "client/bills/conekta/payment"
+        "client/bills/conekta/webhook"
     ];
 }
