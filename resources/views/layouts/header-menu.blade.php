c8f6abb43b3b715faec52717562f7890c19f1bce
Jignesh Prajapati <jignesh.prajapati@plutustec.com>
2022-01-24 10:33:37 +0530

    hot-fix-message-issue

--- resources/views/layouts/header-menu.blade.php
+++ resources/views/layouts/header-menu.blade.php
@@ -19,12 +19,12 @@
                 <div class="user col align-self-end">
                   
                     <?php
-                        echo substr(Auth::user()->first_name,0,15);
+                        echo substr(auth()->user()->first_name,0,15);
                         echo "&nbsp;";
-                        echo substr(Auth::user()->last_name,0,15);
+                        echo substr(auth()->user()->last_name,0,15);
                     ?>
-                    @if(file_exists( public_path().'/images/users/'.Auth::user()->profile_image ) && Auth::user()->profile_image!='' && Auth::User()->is_published=="yes")
-                    <img src="{{URL::asset('/public/images/users/')}}/{{Auth::user()->profile_image}}" id="userDropdown" alt="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    @if(file_exists( public_path().'/images/users/'.auth()->user()->profile_image ) && auth()->user()->profile_image!='' && auth()->user()->is_published=="yes")
+                    <img src="{{URL::asset('/public/images/users/')}}/{{auth()->user()->profile_image}}" id="userDropdown" alt="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 
                     @else
                     <img src="{{asset('assets/images/faces/default_face.svg')}}" id="userDropdown" alt=""
@@ -32,18 +32,18 @@
                     @endif
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
                         <div class="dropdown-header">
-                            <?php if(isset(Auth::user()->first_name)){?>
+                            <?php if(isset(auth()->user()->first_name)){?>
                             <i class="i-Lock-User mr-1"></i>
                             <?php
-                                echo substr(Auth::user()->first_name,0,15);
+                                echo substr(auth()->user()->first_name,0,15);
                                 echo "&nbsp;";
-                                echo substr(Auth::user()->last_name,0,15);
+                                echo substr(auth()->user()->last_name,0,15);
                             }?>
                         </div>
                         <a class="dropdown-item" href="{{ route('account/dashboard') }}" >All Settings</a>
                         <a class="dropdown-item" href="{{ route('load_profile') }}" >My Profile & Contact Info</a>
                         @can('add_firm_user')
-                        <a class="dropdown-item" >Add Firm User</a>    
+                        <a class="dropdown-item m2" >Add Firm User</a>    
                         @endcan
                         @can('edit_firm_user_permission')
                         <a class="dropdown-item" href="{{ route('contacts/attorneys') }}" >Firm User Permissions</a>
