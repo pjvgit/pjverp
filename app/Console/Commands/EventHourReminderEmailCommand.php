97c405ec5b43542ed04689b960e35c309fe5774f
Trupti Bhuva <trupti.bhuva@plutustec.com>
2022-04-11 10:57:08 +0530

    event email reminder changes

--- app/Console/Commands/EventHourReminderEmailCommand.php
+++ app/Console/Commands/EventHourReminderEmailCommand.php
@@ -74,7 +74,7 @@ class EventHourReminderEmailCommand extends Command
                         $eventStartTime = Carbon::parse($item->start_date.' '.$item->event->start_time);
                         Log::info("hour event start time: ". $eventStartTime);
                         $remindTime = Carbon::parse($eventStartTime)->subHours($ritem->reminer_number);
-                        Log::info("hour event remind time: ". $remindTime);
+                        Log::info("hour event remind time: ". $remindTime->format('Y-m-d H:i'));
                         $dispatchDate = Carbon::createFromFormat('Y-m-d H:i', $remindTime);
                         Log::info("EventHourReminderEmailCommand : hour time true");
                         dispatch(new EventReminderEmailJob($item, $users, $attendEvent))->delay($dispatchDate);
