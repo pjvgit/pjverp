fb6e886e9307ff3fc222c396c651aa5b9122e849
Trupti Bhuva <trupti.bhuva@plutustec.com>
2022-05-05 20:48:30 +0530

    invoice module changes and bug fixing

--- resources/views/billing/invoices/invoices.blade.php
+++ resources/views/billing/invoices/invoices.blade.php
@@ -388,99 +388,6 @@ td,th{
 
 @include('billing.invoices.partials.invoice_action_modal')
 
-{{-- <div id="sendInvoiceReminder" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog"
-    aria-labelledby="exampleModalCenterTitle" aria-hidden="true" data-keyboard="false" data-backdrop="static">
-    <div class="modal-dialog  modal-lg ">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="depostifundtitle">Send Reminder</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span
-                        aria-hidden="true">×</span>
-                    </button>
-            </div>
-            <div class="modal-body">
-                <span class="showError"></span>
-                <div id="sendInvoiceReminderArea">
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<div id="payInvoicePopup" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog"
-    aria-labelledby="exampleModalCenterTitle" aria-hidden="true" data-keyboard="false" data-backdrop="static">
-    <div class="modal-dialog  modal-lg ">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="depostifundtitle">Pay Invoice</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span
-                        aria-hidden="true">×</span>
-                    </button>
-            </div>
-            <div class="modal-body">
-                <span class="showError"></span>
-                <div id="payInvoicePopupArea">
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- <div id="payInvoice" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog"
-    aria-labelledby="exampleModalCenterTitle" aria-hidden="true" data-keyboard="false" data-backdrop="static">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalCenterTitle">Record Payment</h5>
-                <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span
-                        aria-hidden="true">×</span></button>
-            </div>
-            <div class="modal-body">
-                <div class="row">
-                    <div class="col-md-12">
-                        <span class="showError"></span>
-                        <div id="payInvoiceArea">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> -->
-<div id="deleteInvoice" class="modal fade show modal-overlay" tabindex="-1" role="dialog"
-    aria-labelledby="exampleModalCenterTitle" aria-hidden="true" data-keyboard="false" data-backdrop="static">
-    <div class="modal-dialog">
-        <form class="deleteInvoiceForm" id="deleteInvoiceForm" name="deleteInvoiceForm" method="POST">
-            <input type="hidden" id="delete_invoice_id" name="invoice_id">
-            @csrf
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalCenterTitle">Confirm Delete</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span
-                            aria-hidden="true">×</span></button>
-                </div>
-                <div class="modal-body">
-                    <div class="showError" style="display:none"></div>
-                    <div class="row">
-                        <div class="col-md-12" id="confirmAccess">
-                            Are you sure you want to delete this Invoice?
-                        </div>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <div class="col-md-12  text-center">
-                        <div class="loader-bubble loader-bubble-primary innerLoader" id="innerLoader"
-                            style="display: none;"></div>
-                        <div class="form-group row float-right">
-                            <button class="btn btn-secondary m-1" type="button" data-dismiss="modal">Cancel</button>
-                            <button class="btn btn-primary ladda-button example-button m-1 submit" id="submit"
-                                type="submit">Delete</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </form>
-    </div>
-</div> --}}
-
 <div id="deleteBulkInvoice" class="modal fade show modal-overlay" tabindex="-1" role="dialog"
     aria-labelledby="exampleModalCenterTitle" aria-hidden="true" data-keyboard="false" data-backdrop="static">
     <div class="modal-dialog">
@@ -496,7 +403,13 @@ td,th{
                     <div class="showError" style="display:none"></div>
                     <div class="row">
                         <div class="col-md-12" id="confirmAccess">
-                            Are you sure you want to delete the selected invoices?
+                            <p>Are you sure you want to delete this Invoice?</p>
+                            <ul>
+                                <li>This action can’t be undone</li>
+                                <li>Time entries, expenses and flat fee entries will be put back into the system in their "Open" state.</li>
+                                <li>All payments and refunds made against the invoice WILL BE DELETED. If trust funds were used to pay the invoice, those funds will be returned to the client's trust fund. Other funds with different origin will be sent to the client's trust fund as well.</li>
+                                <li>Firm users working on the case will receive a notification</li>
+                            </ul>
                         </div>
                     </div>
                 </div>
