9e5f1509cfce2907627befb0ec851e0cfeaa23ec
Trupti Bhuva <trupti.bhuva@plutustec.com>
2022-08-08 12:39:45 -0500

    bug fixing

--- app/InvoiceOnlinePayment.php
+++ app/InvoiceOnlinePayment.php
@@ -24,8 +24,8 @@ class InvoiceOnlinePayment extends Model
     public function getExpiresDateAttribute()
     {
         // return Carbon::parse($this->conekta_reference_expires_at)->format('d-m-Y');
-        $userTime = convertToUserTimezone($this->conekta_reference_expires_at, auth()->user()->user_timezone ?? 'UTC');
-        return $userTime->format('d-m-Y');
+        // $userTime = convertToUserTimezone($this->conekta_reference_expires_at, auth()->user()->user_timezone ?? 'UTC');
+        // return $userTime->format('d-m-Y');
     }
 
     /**
@@ -34,8 +34,8 @@ class InvoiceOnlinePayment extends Model
     public function getExpiresTimeAttribute()
     {
         // return Carbon::parse($this->conekta_reference_expires_at)->format('H:i');
-        $userTime = convertToUserTimezone($this->conekta_reference_expires_at, auth()->user()->user_timezone ?? 'UTC');
-        return $userTime->format('H:i');
+        // $userTime = convertToUserTimezone($this->conekta_reference_expires_at, auth()->user()->user_timezone ?? 'UTC');
+        // return $userTime->format('H:i');
     }
 
     /**
